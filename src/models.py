683c61a3d3aac5d48ea400de75c769de57b7e75e
Luis <166702635+LuisIannelloDev@users.noreply.github.com>
2024-07-17 12:56:20 +0000

    Data Modeling Luis

--- src/models.py
+++ src/models.py
@@ -13,16 +13,36 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
 
 class Address(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    post_code = Column(String(250), nullable=False)
     street_name = Column(String(250))
     street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_to_id = Column(String(250), nullable=False)
+
+class Followers(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key=True)
     person_id = Column(Integer, ForeignKey('person.id'))
+    user_to_id = Column(String(250), nullable=False)
+   
+    
     person = relationship(Person)
 
     def to_dict(self):
